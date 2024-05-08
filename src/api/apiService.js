63648483b25175893f84a3ddb567f665802740d6
Nathalia Fernandes <nathalia.fernandes@toptal.com>
2024-05-08 18:00:00 -0300

    Change base url again

--- src/api/apiService.js
+++ src/api/apiService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: `http://${process.env.HOST}:${process.env.PORT}`,
+  baseURL: `https://${process.env.HEROKU_APP_NAME}.herokuapp.com`,
 });
 
 export const uploadImage = async (file) => {
