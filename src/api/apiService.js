0dcb367e9a0e55eb9a8a33693bd44be079a8482d
Nathalia Fernandes <nathalia.fernandes@toptal.com>
2024-05-08 17:51:58 -0300

    Testing something

--- src/api/apiService.js
+++ src/api/apiService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: `https://perdidogs-408fa6504128.herokuapp.com`,
+  baseURL: `http://${process.env.HOST}:${process.env.PORT}`,
 });
 
 export const uploadImage = async (file) => {
